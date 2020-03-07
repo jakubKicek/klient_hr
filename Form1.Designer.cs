1e39bba24f9630e07036143c137ace70cdd35039
jakub <j.kicek@gmail.com>
2020-03-07 11:59:19 +0100

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace HR_To_AWS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RunBTN = new System.Windows.Forms.Button();
+            this.ReadBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RunBTN
+            // 
+            this.RunBTN.Location = new System.Drawing.Point(110, 77);
+            this.RunBTN.Name = "RunBTN";
+            this.RunBTN.Size = new System.Drawing.Size(75, 23);
+            this.RunBTN.TabIndex = 0;
+            this.RunBTN.Text = "Run";
+            this.RunBTN.UseVisualStyleBackColor = true;
+            this.RunBTN.Click += new System.EventHandler(this.RunBTN_Click);
+            // 
+            // ReadBTN
+            // 
+            this.ReadBTN.Location = new System.Drawing.Point(110, 123);
+            this.ReadBTN.Name = "ReadBTN";
+            this.ReadBTN.Size = new System.Drawing.Size(75, 23);
+            this.ReadBTN.TabIndex = 1;
+            this.ReadBTN.Text = "Read";
+            this.ReadBTN.UseVisualStyleBackColor = true;
+            this.ReadBTN.Click += new System.EventHandler(this.ReadBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReadBTN);
+            this.Controls.Add(this.RunBTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RunBTN;
+        private System.Windows.Forms.Button ReadBTN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HR_To_AWS
+{
+    public partial class Form1 : Form
+    {
+        DEMO_KD_1Entities context = new DEMO_KD_1Entities();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void RunBTN_Click(object sender, EventArgs e)
+        {
+
+            // var ActiveInstance = BaseContext.PRAC_EVENT_INSTANCE_TO_PORTION(secondParametr).ToArray<Model.PRAC_EVENT_INSTANCE_TO_PORTION_Result>();
+            // var Resp = ActiveInstance.First();
+
+            var ProcState = context.AWS_PRAC_TO_VECTOR();//.ToArray<>(DEMO_KD_1Model.Store.AWS_PRAC_TO_VECTOR);
+            //var Resp = ActiveInstance.First();
+        }
+        private void ReadBTN_Click(object sender, EventArgs e)
+        {
+            AWS_PRAC_WEKTOR emp = context.AWS_PRAC_WEKTOR.FirstOrDefault();
+            MessageBox.Show(emp.id.ToString());
+        }
+    }
+}
